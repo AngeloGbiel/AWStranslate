a85188c11fd406e4c8045de1fd375e5467e5b742
AngeloGbiel <angelog.iff@gmail.com>
2023-12-09 15:29:54 -0300

    modificando a porta do host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server:{
-    host: true,
-    strictPort: true,
-    port: 4000
-  }
+  plugins: [react()]
 })
